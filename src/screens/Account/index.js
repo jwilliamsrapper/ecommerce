58c82a97eb4a86313604df0612a74d33d435d261
root <root@localhost.localdomain>
2020-02-23 18:50:24 +0000

    changed for ipda

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -37,7 +37,7 @@ export default class Account extends React.Component {
         this.setState({loading: true})
         await AsyncStorage.removeItem("uid");
         await SignOut();
-        this.props.navigation.navigate("Auth")
+        this.props.navigation.navigate("Home")
     }
     render() {
         const {loading, userName, image, vendor} = this.state;
